1d39d603eeb01a14307c43c89162c2a9bb12d422
MrAceitun0 <aceitunodev@gmail.com>
2022-10-05 18:20:44 +0200

    Death by void

---
+++ LudumDare51/Assets/Scripts/FirstPersonController/Components/Respawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    private const string IS_DEAD = "isDead";
+    [SerializeField]
+    Animator killScreenAnimator;
+
+    bool isDead = false;
+
+    public bool getIsDead()
+    {
+        return isDead;
+    }
+
+    public void setIsDead(bool isDead)
+    {
+        this.isDead = isDead;
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            killScreenAnimator.SetBool(IS_DEAD, true);
+        }
+    }
+
+    public IEnumerator respawnPlayer(Transform playerTransform)
+    {
+        yield return new WaitForSeconds(0.8f);
+        playerTransform.position = transform.position;
+        transform.rotation = transform.rotation;
+        isDead = false;
+        killScreenAnimator.SetBool(IS_DEAD, false);
+    }
+}
