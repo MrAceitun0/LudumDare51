4b45f1df0e1824147ce185390742ea7396a5f8a8
MrAceitun0 <aceitunodev@gmail.com>
2022-10-05 20:36:45 +0200

    Lasers Working Fine

---
+++ LudumDare51/Assets/Scripts/FirstPersonController/Components/LaserCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserCollision : MonoBehaviour
+{
+
+    Respawner respawner;
+
+    private void Start()
+    {
+        respawner = FindObjectOfType<Respawner>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Laser")
+        {
+            respawner.setIsDead(true);
+            StartCoroutine(respawner.respawnPlayer(transform));
+        }
+    }
+}
--- LudumDare51/Assets/Scripts/FirstPersonController/FirstPersonMovement.cs
+++ LudumDare51/Assets/Scripts/FirstPersonController/FirstPersonMovement.cs
@@ -5,11 +5,20 @@ public class FirstPersonMovement : MonoBehaviour
     public float speed = 5;
     Vector2 velocity;
 
+    Respawner respawner;
+
+    private void Start()
+    {
+        respawner = FindObjectOfType<Respawner>();
+    }
 
     void Update()
     {
-        velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        transform.Translate(velocity.x, 0, velocity.y);
+        if (!respawner.getIsDead())
+        {
+            velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.Translate(velocity.x, 0, velocity.y);
+        }
     }
 }
