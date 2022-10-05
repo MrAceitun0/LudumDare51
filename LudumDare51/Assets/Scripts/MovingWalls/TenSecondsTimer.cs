ec8e185859c4a672fb3818244749971511c83676
MrAceitun0 <aceitunodev@gmail.com>
2022-10-05 08:33:32 +0200

    Moving Walls

---
+++ LudumDare51/Assets/Scripts/MovingWalls/TenSecondsTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenSecondsTimer : MonoBehaviour
+{
+    [SerializeField]
+    int totalPhasesInLevel;
+
+    private int phaseCounter = 0;
+
+    private float phaseTime = 10f;
+    private float remainingTime;
+
+    private void Start()
+    {
+        remainingTime = phaseTime;
+    }
+
+    void Update()
+    {
+        if(remainingTime < 0)
+        {
+            WallMovement[] movingWalls = FindObjectsOfType<WallMovement>();
+            foreach(WallMovement wall in movingWalls)
+            {
+                wall.moveWall(phaseCounter % totalPhasesInLevel);
+            }
+
+            phaseCounter++;
+
+            remainingTime = phaseTime;
+        }
+        
+        remainingTime -= Time.deltaTime;
+    }
+}
---
+++ LudumDare51/Assets/Scripts/MovingWalls/WallMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour
+{
+    [SerializeField]
+    int closedPhase;
+
+    [SerializeField]
+    float closedDoorYValue;
+
+    [SerializeField]
+    float openDoorYValue;
+
+    [SerializeField]
+    float movementTime;
+
+    public void moveWall(float phase)
+    {
+        if(phase == closedPhase)
+        {
+            Debug.Log(phase);
+            closeDoor();
+        } 
+        else
+        {
+            openDoor();
+        }
+    }
+
+    private void closeDoor()
+    {
+        StartCoroutine(LerpFunction(closedDoorYValue, movementTime));
+    }
+
+    private void openDoor()
+    {
+        StartCoroutine(LerpFunction(openDoorYValue, movementTime));
+    }
+
+    IEnumerator LerpFunction(float endValue, float duration)
+    {
+        float time = 0;
+        float startValue = transform.position.y;
+
+        while (time < duration)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(startValue, endValue, time / duration), transform.position.z);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = new Vector3(transform.position.x, endValue, transform.position.z);
+    }
+}
