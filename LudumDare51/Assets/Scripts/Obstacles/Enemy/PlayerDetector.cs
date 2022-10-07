39ed27b42030fb794fcf6b5286442b1daeb63622
MrAceitun0 <aceitunodev@gmail.com>
2022-10-07 20:29:09 +0200

    Enemic acabat

---
+++ LudumDare51/Assets/Scripts/Obstacles/Enemy/PlayerDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+
+    bool isPlayerDetected = false;
+
+    public bool getIsPlayerDetected()
+    {
+        return isPlayerDetected;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isPlayerDetected = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isPlayerDetected = false;
+        }
+    }
+}
