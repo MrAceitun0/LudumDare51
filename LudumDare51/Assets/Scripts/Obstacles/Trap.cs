f676084595d2df02ab1c5a500efddb3b1663efc8
MrAceitun0 <aceitunodev@gmail.com>
2022-10-07 17:33:33 +0200

    Traps working fine

---
+++ LudumDare51/Assets/Scripts/Obstacles/Trap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    Transform initialTransform;
+    Respawner respawner;
+
+    private void Start()
+    {
+        respawner = FindObjectOfType<Respawner>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponentInChildren<ParticleSystem>().Play();
+            respawner.setIsDead(true);
+            StartCoroutine(respawner.respawnPlayer(other.transform));
+        }
+    }
+}
