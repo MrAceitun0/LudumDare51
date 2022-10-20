f187d1615d6b3d947e3128524f469e0fad1caa40
MrAceitun0 <aceitunodev@gmail.com>
2022-10-20 14:01:32 +0200

    Audios and finishing obstacles Lvl2

--- LudumDare51/Assets/Scripts/FirstPersonController/Components/LaserCollision.cs
+++ LudumDare51/Assets/Scripts/FirstPersonController/Components/LaserCollision.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class LaserCollision : MonoBehaviour
 {
-
+    AudioSource audioSource;
+    public AudioClip laserDeath;
     Respawner respawner;
 
     private void Start()
     {
         respawner = FindObjectOfType<Respawner>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Laser")
         {
+            audioSource.PlayOneShot(laserDeath);
             respawner.setIsDead(true);
             StartCoroutine(respawner.respawnPlayer(transform));
         }
